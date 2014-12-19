4e5eb9b1dea6aeb1bc7d40da8b7db21eb0e4ba01
Leben Asa <lebenasa@hotmail.com>
2014-12-19 13:50:43 +0700

    - Created first step implementation for SerialCapture

--- SerialCapture/serialcapture.cpp
+++ SerialCapture/serialcapture.cpp
@@ -1,13 +1,86 @@
 #include "stdafx.h"
 #include "serialcapture.h"
+#include "camera.h"
+#include "stepper.h"
+#include "cameramodel.h"
+#include "sminterface.h"
+using namespace std;
 
 SerialCapture::SerialCapture(QObject *parent)
-	: QObject(parent)
+	: QObject(parent), engine{ this }, m_zoom(10), m_block(false)
 {
+	// Initialize
+	m_camera = new DSCamera(this);
+	m_stepper = new CNCStepper(this);
+	m_interface = new SMInterface(0, this);
+	m_model = new CameraModel(m_interface->rows(), m_interface->cols(), this);
+	rootContext = engine.rootContext();
 
+	m_size = m_camera->size() / m_zoom;
+
+	// Connections
+	connect(m_stepper, &Stepper::xyChanged, m_interface, &SMInterface::updatePos);
+	connect(m_stepper, &Stepper::bufferFull, this, &SerialCapture::unblockStream);
+	connect(m_camera, &DSCamera::frameReady, this, &SerialCapture::redirectImage);
+
+	// Start UI
+	rootContext->setContextProperty("serialcapture", this);
+	rootContext->setContextProperty("camera", m_camera);
+	rootContext->setContextProperty("stepper", m_stepper);
+	rootContext->setContextProperty("model", m_model);
+	engine.load(QUrl(QStringLiteral("qrc:///Resources/SerialCapture/SerialCapture.qml")));
 }
 
 SerialCapture::~SerialCapture()
 {
 
+}
+
+void SerialCapture::blockStream() {
+	if (!m_block) {
+		m_block = true;
+		emit blockChanged(true);
+	}
+}
+
+void SerialCapture::unblockStream() {
+	if (m_block) {
+		m_block = false;
+		emit blockChanged(false);
+	}
+}
+
+void SerialCapture::redirectImage(const QImage& img) {
+	if (!m_block && !m_stepper->isWorking())
+		m_model->updateBuffer(img, m_interface->currentPos());
+}
+
+QSize SerialCapture::cellSize() const {
+	return m_size;
+}
+
+void SerialCapture::setCellSize(const QSize& size) {
+	if (m_size != size) {
+		m_size = size;
+		emit cellSizeChanged(m_size);
+	}
+}
+
+void SerialCapture::zoomIn() {
+	if (m_zoom > 1) {
+		--m_zoom;
+		setCellSize(m_camera->size() / m_zoom);
+	}
+}
+
+void SerialCapture::zoomOut() {
+	if (m_zoom < 10) {
+		++m_zoom;
+		setCellSize(m_camera->size() / m_zoom);
+	}
+}
+
+void SerialCapture::moveToSelected() {
+	blockStream();
+	m_interface->moveTo(m_model->selectedCell());
 }
--- SerialCapture/serialcapture.h
+++ SerialCapture/serialcapture.h
@@ -2,17 +2,52 @@
 #define SERIALCAPTURE_H
 
 #include <QObject>
+#include "qqmlapplicationengine.h"
+#include "qqmlcontext.h"
+
+class DSCamera;
+class Stepper;
+class CameraModel;
+class SMInterface;
 
 class SerialCapture : public QObject
 {
 	Q_OBJECT
-
+	Q_PROPERTY(QSize cellSize READ cellSize NOTIFY cellSizeChanged)
+	Q_PROPERTY(bool block MEMBER m_block NOTIFY blockChanged)
+	
 public:
-	SerialCapture(QObject *parent);
+	SerialCapture(QObject *parent = 0);
 	~SerialCapture();
 
+	QSize cellSize() const;
+	void setCellSize(const QSize &size);
+
+public slots:
+	void zoomIn();
+	void zoomOut();
+
+	void blockStream();
+	void unblockStream();
+	void redirectImage(const QImage& img);
+
+	void moveToSelected();
+
+signals:
+	void cellSizeChanged(const QSize &size);
+	void blockChanged(bool);
+
 private:
-	
+	DSCamera* m_camera;
+	Stepper* m_stepper;
+	CameraModel* m_model;
+	SMInterface* m_interface;
+	QQmlApplicationEngine engine;
+	QQmlContext* rootContext;
+
+	QSize m_size;	// cell size, i.e for implementing zoom
+	int m_zoom;
+	bool m_block;
 };
 
 #endif // SERIALCAPTURE_H
